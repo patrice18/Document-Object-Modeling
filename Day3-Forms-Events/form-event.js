c3dda3532fb4fcb264334097c1a7bf2a1434b75f
Patrice Thomas <patrice.thomas1229@gmail.com>
2018-07-07 17:15:06 -0400

    Added user values in forms and create object, stored in array with these new values

--- Day3-Forms-Events/form-event.js
+++ Day3-Forms-Events/form-event.js
@@ -9,16 +9,30 @@ let Store = function (location, Average_Cookies_Per_Customer,Avg_Number_Of_Custo
     };
 }
 
+
 //selecting  the element node I want the script to respond to. This variable is facilitating DOM access to the form.
-elStore = document.getElementByID("stores");
+let elStore = document.getElementById("stores");
 
 // stating the input field my form needs and making their value equal to the different parameters/keys in my object constructor.
-let userSelect = Store.location;
-let userNumber = Store.Average_Cookies_Per_Customer;
-let UserAvgofCustomer = Store.Avg_Number_Of_Customers;
+let userSelect = elStore.location1;
+let userNumber = elStore.avg_cookies_per_cust;
+let UserAvgofCustomer = elStore.avgOfCustomer;
 
 
+//creating an array as placeholder to store the new objects that is create when user puts required values and fire the event "subscribe"
+let storeArray = [];
 
-//indicating which event on the form node will trigger the respo
+//indicating which event on the form node will trigger the response
+//stating the function I want to run when event "submit" is fired
+elStore.addEventListener('submit', function(event) {
+    event.preventDefault()
+    
+    //creating new instance of the Store Constructor
+ let newStore = new Store(userSelect.value, userNumber.value, UserAvgofCustomer.value)  
+storeArray.push(newStore)
+console.log(newStore);
+console.log(storeArray);
 
-//stating the function I want to run when the event (...) happended
+});
+
+
